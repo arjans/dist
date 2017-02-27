c68fad996ba749cebcfcf654601e990128401581
Arjan Singh <arjansingh1989@gmail.com>
2017-02-26 17:00:26 -0800

    Add files for racket packages.

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "dist")
+(define deps '("base"
+               "rackunit-lib"))
+(define pkg-desc "Exploring distance fields using plot.")
+(define version "0.0")
+(define pkg-authors '(arjan))
